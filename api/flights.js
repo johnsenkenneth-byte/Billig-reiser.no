ba46db9fa25c2e9387ea1344383d21dd6304917a
johnsenkenneth-byte <johnsenkenneth@icloud.com>
2026-04-29 15:52:56 +0200

    Delete api directory

--- api/flights.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    flights: [],
-    error: "Amadeus API-route er klar. Legg API-nøkler i Vercel når du får dem."
-  });
-}
